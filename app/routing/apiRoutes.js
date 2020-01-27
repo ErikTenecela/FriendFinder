d0a995a3803992b9d6bdb21683762fb5e32c9a12
Erik Tenecela <eriktenecela13@gmail.com>
2020-01-27 14:16:55 -0500

    clear-inputs

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,64 +1,50 @@
-const friends = require('./../data/friends')
+const friends = require("./../data/friends");
 
-module.exports = function(app) {
-    app.get("/api/friends", function(req, res) {
-        res.json(friends);
+module.exports = app => {
+  app.get("/api/friends", (req, res) => {
+    res.json(friends);
+  });
+
+  app.post("/api/friends", (req, res) => {
+    let totalDifference = 0;
+
+    let bestMatch = {
+      name: "",
+      photo: "",
+      friendDifference: 1000
+    };
+
+    let userData = req.body;
+    let userName = userData.name;
+    let userScores = userData.scores;
+
+    let b = userScores.map(item => {
+      return parseInt(item, 10);
     });
+    userData = {
+      name: req.body.name,
+      photo: req.body.photo,
+      scores: b
+    };
 
-    app.post("/api/friends", function(req, res) {
-        let totalDifference = 0;
+    let sum = b.reduce((a, b) => a + b, 0);
 
-        let bestMatch = {
-            name: "",
-            photo: "",
-            friendDifference: 1000
-        };
+    for (var i = 0; i < friends.length; i++) {
+      totalDifference = 0;
 
-        let userData = req.body;
-        let userName = userData.name;
-        let userScores = userData.scores;
+      var bfriendScore = friends[i].scores.reduce((a, b) => a + b, 0);
 
-        let b = userScores.map(function(item) {
-            return parseInt(item, 10);
-        });
-        userData = {
-            name: req.body.name,
-            photo: req.body.photo,
-            scores: b
-        };
+      totalDifference += Math.abs(sum - bfriendScore);
 
-        console.log("Name: " + userName);
-        console.log("User Score " + userScores);
+      if (totalDifference <= bestMatch.friendDifference) {
+        bestMatch.name = friends[i].name;
+        bestMatch.photo = friends[i].photo;
+        bestMatch.friendDifference = totalDifference;
+      }
+    }
 
-        let sum = b.reduce((a, b) => a + b, 0);
+    friends.push(userData);
 
-        console.log("Sum of users score " + sum);
-        console.log("Best match friend diff " + bestMatch.friendDifference);
-        console.log("+++++++=================++++++++++");
-
-        for (var i = 0; i < friends.length; i++) {
-            console.log(friends[i].name);
-            totalDifference = 0;
-            console.log("Total Diff " + totalDifference);
-            console.log("Best match friend diff " + bestMatch.friendDifference);
-
-            var bfriendScore = friends[i].scores.reduce((a, b) => a + b, 0);
-            console.log("Total friend score " + bfriendScore);
-            totalDifference += Math.abs(sum - bfriendScore);
-            console.log("-------------------------> " + totalDifference);
-
-            if (totalDifference <= bestMatch.friendDifference) {
-                bestMatch.name = friends[i].name;
-                bestMatch.photo = friends[i].photo;
-                bestMatch.friendDifference = totalDifference;
-            }
-            console.log(totalDifference + " Total Difference");
-        }
-        console.log(bestMatch);
-
-        friends.push(userData);
-        console.log("New user added");
-        console.log(userData);
-        res.json(bestMatch);
-    });
+    res.json(bestMatch);
+  });
 };
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,15 +1,14 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-
 module.exports = app => {
-    app.get("/survey", function(req, res) {
-        res.sendFile(path.join(__dirname + "/../public/survey.html"));
-    });
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname + "/../public/survey.html"));
+  });
 
-    app.use(function(req, res) {
-        res.sendFile(path.join(__dirname + "/../public/home.html"));
-    });
-}
+  app.use("/", function(req, res) {
+    res.sendFile(path.join(__dirname + "/../public/home.html"));
+  });
+};
