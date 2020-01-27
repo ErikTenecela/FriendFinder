1e819c9046e06621170537a6e6c7e58f485d7845
Erik Tenecela <eriktenecela13@gmail.com>
2020-01-27 14:27:38 -0500

    updated-data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,37 +1,45 @@
-const friends = [{
-        name: "Tom Hardy",
-        photo: "https://m.media-amazon.com/images/M/MV5BMTQ3ODEyNjA4Nl5BMl5BanBnXkFtZTgwMTE4ODMyMjE@._V1_SY1000_CR0,0,666,1000_AL_.jpg",
-        scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
-    },
-    {
-        name: "Jamie Dornan",
-        photo: "https://www.usmagazine.com/wp-content/uploads/2018/02/jamie-dornan-nude.jpg",
-        scores: [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
-    },
-    {
-        name: "Anthony Joshua",
-        photo: "https://m.media-amazon.com/images/M/MV5BYWFlMmVhZjAtOGMzNS00MzEyLTkzMWEtN2M1MDgzODI5Njk0XkEyXkFqcGdeQXVyNTI5NjIyMw@@._V1_.jpg",
-        scores: [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
-    },
-    {
-        name: "Henry Cavill",
-        photo: "https://m.media-amazon.com/images/M/MV5BMTUxNTExMzUzOF5BMl5BanBnXkFtZTgwOTI1MjA3OTE@._V1_.jpg",
-        scores: [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
-    },
-    {
-        name: "Emily Ratajowski",
-        photo: "https://pmchollywoodlife.files.wordpress.com/2014/10/Emily-Ratajkowski-Reveals-Rock-Hard-Abs-In-Teeny-Crop-Top-While-Vacationing-With-Husband-gallery.jpg",
-        scores: [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
-    },
-    {
-        name: "Blake Lively",
-        photo: "https://media.vanityfair.com/photos/560c07690404673e0de66e1d/master/w_960,c_limit/blake-lively-end-of-preserve-01.jpg",
-        scores: [4, 3, 4, 2, 4, 3, 5, 3, 5, 4]
-    },
-    {
-        name: "Kate Upton",
-        photo: "https://m.media-amazon.com/images/M/MV5BMTk3MTA2ODQ1MF5BMl5BanBnXkFtZTcwMjkwMDYxNw@@._V1_SY1000_CR0,0,666,1000_AL_.jpg",
-        scores: [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
-    }
+const friends = [
+  {
+    name: "Hello World",
+    photo:
+      "https://i0.wp.com/www.cmo4hire.com/wp-content/uploads/2016/10/Blog-background-hello-world.jpg?fit=1200%2C800",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+  },
+  {
+    name: "Bill Gates",
+    photo:
+      "https://www.bing.com/th?id=OIP.1OedFHa3WFJumyXy5cktBwHaE2&pid=Api&rs=1",
+    scores: [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+  },
+  {
+    name: "Anthony Joshua",
+    photo:
+      "https://m.media-amazon.com/images/M/MV5BYWFlMmVhZjAtOGMzNS00MzEyLTkzMWEtN2M1MDgzODI5Njk0XkEyXkFqcGdeQXVyNTI5NjIyMw@@._V1_.jpg",
+    scores: [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
+  },
+  {
+    name: "Kobe Bryant",
+    photo:
+      "https://pixel.nymag.com/imgs/daily/intelligencer/2016/04/13/13-kobe-bryant-2016.w700.h700.jpg",
+    scores: [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
+  },
+  {
+    name: "Naruto",
+    photo:
+      "https://static0.cbrimages.com/wordpress/wp-content/uploads/2018/11/Naruto-cover.jpg",
+    scores: [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
+  },
+  {
+    name: "Rick and morty",
+    photo:
+      "https://vignette.wikia.nocookie.net/rickandmorty/images/c/c3/The_Smith_Family.jpg/revision/latest?cb=20180201010803",
+    scores: [4, 3, 4, 2, 4, 3, 5, 3, 5, 4]
+  },
+  {
+    name: "Steve Jobs ",
+    photo:
+      "https://www.bing.com/th?id=OIP.R9zpEkHIIDvF_tybEhEcVgHaF7&pid=Api&rs=1",
+    scores: [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
+  }
 ];
 module.exports = friends;
